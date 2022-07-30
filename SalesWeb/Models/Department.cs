bf125ec39a1ba5dfcfe4e2b8fa436971dea6a636
Fabio Freire <fabiofreire84@gmail.com>
2022-07-30 11:52:56 -0300

    Second Migration

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SalesWeb.Models
 {
@@ -9,5 +10,24 @@ namespace SalesWeb.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller (Seller Seller)
+        {
+            Sellers.Add(Seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
